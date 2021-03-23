6dcf1ac1bc6f3c1e01832b45a9ab7be644afb343
Yishayahu <ishayahu156@gmail.com>
2021-03-23 23:12:45 +0200

    save res to file

--- regular_sampler.py
+++ regular_sampler.py
@@ -8,21 +8,26 @@ class RegularSampler(torch.utils.data.Sampler):
         self.clustering_indexes = clustering_indexes
         self.warmup_epochs =warmup_epochs
         self._clustering_flag = "training"
+        self.counter = 0
     def get_clustering_flag(self):
         return self._clustering_flag
     def __iter__(self):
-        for _ in range(self.warmup_epochs+1):
+        if self.counter < self.warmup_epochs+1:
+            self.couter+=1
             indexes = self.train_indexes
             random.shuffle(indexes)
             for idx in indexes:
                 yield idx
-        self._clustering_flag = "clustering"
-        indexes = self.clustering_indexes
-        random.shuffle(indexes)
-        for idx in indexes:
-            yield idx
-        self._clustering_flag = "done"
-        for i in range(8):
-            yield  random.choice(self.clustering_indexes)
+        if self.counter == self.warmup_epochs+1:
+            self._clustering_flag = "clustering"
+            indexes = self.clustering_indexes
+            random.shuffle(indexes)
+            for idx in indexes:
+                yield idx
+            self._clustering_flag = "done"
+            for i in range(8):
+                yield random.choice(self.clustering_indexes)
+
+
     def __len__(self):
         return len(self.ds)
